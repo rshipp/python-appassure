600b8a7c3c7b6d1edebe5a08b1543c3ad98a7225
george <rpubaddr0@gmail.com>
2014-03-17 10:13:21 -0600

    Added IAgentDiagnosticsManagement.

---
+++ appassure/core/IAgentDiagnosticsManagement.py
@@ -0,0 +1,33 @@
+"""AppAssure 5 Core API"""
+
+from appassure.appassureapi import AppAssureAPI
+
+class IAgentDiagnosticsManagement(AppAssureAPI):
+    """Full documentation online at
+    http://docs.appassure.com/display/AA50D/IAgentDiagnosticsManagement
+    """
+
+    def executeRemoteCommand(self, data, agentId):
+        """Executes an arbitrary remote command."""
+        return self.session.request('agentdiag/%s/command/' 
+                % (agentId), 'POST', 
+                    self.getXML(data, 'remoteCommand'))
+
+    def getLog(self, agentId):
+        """Gets the entire contents of the replay.log file."""
+        return self.session.request('agentdiag/%s/log/' 
+                % (agentId))
+
+    def uploadLogSessions(self, agentId):
+        """Uploads the current log session to Gibraltar.
+        (http:// www.gibraltarsoftware.com/)
+        """
+        return self.session.request('agentdiag/%s/log/' 
+                % (agentId), 'POST')
+
+    def restartService(self, agentId):
+        """Stops, forcibly kills (if necessary), and 
+        re-starts the service.
+        """
+        return self.session.request('agentdiag/%s/service/' 
+                % (agentId), 'DELETE')
