3aaa4901a48f4863a826e3387100eba543503854
george <rpubaddr0@gmail.com>
2014-03-17 11:07:36 -0600

    Added IBootCdBuilderManagement.

---
+++ appassure/core/IBootCdBuilderManagement.py
@@ -0,0 +1,29 @@
+"""AppAssure 5 Core API"""
+
+from appassure.appassureapi import AppAssureAPI
+
+class IBootCdBuilderManagement(AppAssureAPI):
+    """Full documentation online at
+    http://docs.appassure.com/display/AA50D/IBootCdBuilderManagement
+    """
+
+    def getDefaultOutputPath(self):
+        """Gets the default path were resulting ISO image 
+        will be put if output path was not specified explicitly.
+        """
+        return self.session.request('bootcdbuilder/defaults')
+
+    def startBuildingIso(self, data):
+        """Starts new ISO image building task."""
+        return self.session.request('bootcdbuilder/start', 'POST', 
+                    self.getXML(data, 'bootCdBuilderRequest'))
+
+    def validateDriverPackages(self, data):
+        """Performs validation of specified driver packages."""
+        return self.session.request('bootcdbuilder/validate', 'POST', 
+                    self.getXML(data, 'driverPackageValidationRequest'))
+
+    def verifyBuildingIsoParameters(self, data):
+        """Verifies that ISO parameters are built properly."""
+        return self.session.request('bootcdbuilder/verify', 'POST', 
+                    self.getXML(data, 'bootCdBuilderParams'))
