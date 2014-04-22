a24bb42951d6238d2ee2668aa89d1dbdcd8e401f
george <rpubaddr0@gmail.com>
2014-04-22 10:35:14 -0600

    Updated __init__ files.

--- appassure/core/__init__.py
+++
@@ -1,4 +0,0 @@
-"""This package contains the AppAssureAPI class and its subclasses,
-which provide direct access to the AppAssure Core API's interfaces
-and services, as described at http://docs.appassure.com/display/AA50D/.
-"""
